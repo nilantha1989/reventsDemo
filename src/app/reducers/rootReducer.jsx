cdbcfd944a0329f8af00cd0aacffe9c112b15d52
nilantha.sac <nilantha.sac@gmail.com>
2020-05-26 21:04:56 -0600

    End of Section 8

--- src/app/reducers/rootReducer.jsx
+++ src/app/reducers/rootReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import testReducer from "../../features/testarea/testReducer";
+import eventReducers from "../../features/event/eventReducers";
 
 const rootReducer = combineReducers({
   test: testReducer,
+  events: eventReducers,
 });
 
 export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
 
 const store = configureStore();
-console.log(store.getState());
+//console.log(store.getState());
 const rootEl = document.getElementById("root");
 let render = () => {
   ReactDOM.render(
